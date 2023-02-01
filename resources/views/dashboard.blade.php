848b4fa6f205825ea4bc2aa5dddd130ca8df839e
Muhammad Radya Iftikhar <radyaiftikhar@gmail.com>
2023-02-01 14:12:12 +0700

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Leaderboard</title>
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</head>
+<body>
+        <nav class="navbar navbar-expand-lg" style="padding-top:-50px; background-color:red;">
+        <div class=" container-fluid">
+            <a class="navbar-brand" href="#"><img src="logotelkom-putih.png" width="45px" alt=""><span style="color:white">SIAKAD</span></a>
+           
+            <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button> -->
+    <div class="d-flex align-item-right">
+            <input type="button" class="btn btn-light" style="margin-right:10px;" value="2022/2023">
+            <input type="button" class="btn btn-light" value="XI-RPL 2">
+            </div>
+        </div>
+        </nav>
+
+        <!-- Sidebar -->
+        <div id="wrapper">
+
+<!-- Sidebar -->
+<ul class="navbar-nav sidebar sidebar-dark accordion" style="background-color:#282E2F;" id="accordionSidebar">
+
+
+  
+
+    <!-- Nav Item - Utilities Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse"
+            aria-expanded="true" aria-controls="collapseUtilities">
+            <span>Dashboard</span>
+        </a>
+    </li>
+    <li class="nav-item" style="background-color:grey;">
+        <a class="nav-link " href="#" data-toggle="collapse"
+            aria-expanded="true" aria-controls="collapseUtilities">
+            <span>Leaderboard</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse"
+            aria-expanded="true" aria-controls="collapseUtilities">
+            <span>Kehadiran Hari ini</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse"
+            aria-expanded="true" aria-controls="collapseUtilities">
+            <span>Data absensi</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse"
+            aria-expanded="true" aria-controls="collapseUtilities">
+            <span>Jadwal pelajaran</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse"
+            aria-expanded="true" aria-controls="collapseUtilities">
+            <span>Rekap Nilai</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse"
+            aria-expanded="true" aria-controls="collapseUtilities">
+            <span>Pelanggaran</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse"
+            aria-expanded="true" aria-controls="collapseUtilities">
+            <span>SPP</span>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse"
+            aria-expanded="true" aria-controls="collapseUtilities">
+            <span>Prakerin</span>
+        </a>
+    </li>
+</ul>
+<!-- End of Sidebar -->
+    
+<!-- konten -->
+<div class="container">
+    <h4>LEADERBOARD-SMK TELKOM MALANG</h4>
+</div>
+<!-- endkonten -->
+
+</body>
+</html>
