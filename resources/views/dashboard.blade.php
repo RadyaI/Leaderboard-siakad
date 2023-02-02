3ac87b352d38aa6b9179544e7fb45f3d094b5c0e
Muhammad Radya Iftikhar <radyaiftikhar@gmail.com>
2023-02-02 21:31:43 +0700

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,6 +10,10 @@
     <link href="{{ asset('bootstrap/js/bootstrap.bundle.min.js') }}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.min.js"></script>
+
+    <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script> -->
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script> -->
 </head>
 <body>
         <nav class="navbar navbar-expand-lg" style="padding-top:-50px; background-color:red;">
@@ -109,7 +113,7 @@
                             </form> 
                    </div>
                 <div class="card-body">
-                    <table class="table table-hover table-stripped">
+                    <table class="table table-hover table-stripped ">
                         <thead class="bg-dark text-light">
                             <tr>
                                 <th>ID</th>
@@ -125,7 +129,19 @@
                             @php($no=1)
                                <tr role="row">
                                  <th>{{ $data_siswa -> id }}</th>
-                                 <th><a href="" data-bs-toggle="modal" data-bs-target="#exampleModal" style="text-decoration:none; color:black;">{{ $data_siswa -> nama_siswa }}</a></th>
+                                 <th><a href="" 
+                                    id="modalsiswa"
+                                    data-bs-toggle="modal" 
+                                    data-bs-target="#detailsiswa" 
+                                    style="text-decoration:none; color:black;"
+                                    data-nama_siswa="{{ $data_siswa->nama_siswa }}"
+                                    data-angkatan="{{ $data_siswa->angkatan }}"
+                                    data-kelas="{{ $data_siswa->kelas }}"
+                                    data-nisn="{{ $data_siswa->nisn }}"
+                                    data-tgl_lahir="{{ $data_siswa->TempatTanggalLahir }}"
+                                    data-gender="{{ $data_siswa->gender }}"
+                                    data-agama="{{ $data_siswa->agama }}"
+                                 > {{ $data_siswa -> nama_siswa }}</a></th>
                                  <th>{{ $data_siswa -> kelas }}</th>
                                  <th>{{ $data_siswa -> angkatan }}</th>
                                  <th>Sementara Kosong</th>
@@ -144,27 +160,82 @@
 
 <!-- MODAL DETAIL SISWA -->
 
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal fade" id="detailsiswa" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-scrollable">
         <div class="modal-content">
         <div class="modal-header">
             <h1 class="modal-title fs-5" id="exampleModalLabel">Data siswa</h1>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
-        <div class="modal-body">
-            <b>Nama Siswa:</b>
-        </div>
-        <div class="modal-body">
-        <hr>
-           <b>Portofolio Siswa:</b>
-        </div>
+            <div class="modal-body table-responsive">
+              <table class="table table-bordered no-margin">
+                    <tbody>
+                        <tr>
+                            <th>Nama siswa</th>
+                            <td><span id="nama_siswa"></span></td>
+                        </tr>
+                        <tr>
+                            <th>Nisn</th>
+                            <td><span id="nisn"></span></td>
+                        </tr>
+                        <tr>
+                            <th>Angkatan</th>
+                            <td><span id="angkatan"></span></td>
+                        </tr>
+                        <tr>
+                            <th>Kelas</th>
+                            <td><span id="kelas"></span></td>
+                        </tr>
+                        <tr>
+                            <th>Tempat tanggal lahir</th>
+                            <td><span id="TempatTanggalLahir"></span></td>
+                        </tr>
+                        <tr>
+                            <th>Gender</th>
+                            <td><span id="gender"></span></td>
+                        </tr>
+                        <tr>
+                            <th>Agama</th>
+                            <td><span id="agama"></span></td>
+                        </tr>
+                    </tbody>
+              </table>
+            </div>
         <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
+            <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Close</button>
         </div>
         </div>
     </div>
     </div>
 
 </body>
+
+
+
+
+
+
+<script>
+    $(document).ready(function(){
+        $(document).on('click','#modalsiswa',function(){
+            var nama_siswa = $(this).data('nama_siswa');
+            var angkatan = $(this).data('angkatan');
+            var kelas = $(this).data('kelas');
+            var nisn = $(this).data('nisn');
+            var TempatTanggalLahir = $(this).data('TempatTanggalLahir');
+            var gender = $(this).data('gender');
+            var agama = $(this).data('agama');
+            $('#nama_siswa').text(nama_siswa);
+            $('#angkatan').text(angkatan);
+            $('#kelas').text(kelas);
+            $('#nisn').text(nisn);
+            $('#TempatTanggalLahir').text(TempatTanggalLahir);
+            $('#gender').text(gender);
+            $('#agama').text(agama);
+
+        })
+    })
+</script>
+
+
 </html>
